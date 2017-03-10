776238607eac30c11ff19d66b113e7a9f50c0815
Kim Hugener-Ohlsen <kho@hukano.com>
2017-03-10 14:26:52 +0100

    Fixed type parameter names

--- Sources/Sundew.Quantities/Representations/Expressions/IExpressionVisitor.cs
+++ Sources/Sundew.Quantities/Representations/Expressions/IExpressionVisitor.cs
@@ -12,66 +12,66 @@ namespace Sundew.Quantities.Representations.Expressions
     /// <summary>
     /// Interface for implementing the an expression visitor with two parameters.
     /// </summary>
-    /// <typeparam name="TImmutableParameter">The type of the immutable parameter.</typeparam>
-    /// <typeparam name="TMutableParameter">The type of the mutable parameter.</typeparam>
+    /// <typeparam name="TParameter">The type of the immutable parameter.</typeparam>
+    /// <typeparam name="TVariable">The type of the mutable parameter.</typeparam>
     /// <typeparam name="TResult">The type of the result.</typeparam>
-    public interface IExpressionVisitor<in TImmutableParameter, in TMutableParameter, out TResult> :
-        IVisitor<Expression, TImmutableParameter, TMutableParameter, TResult>
+    public interface IExpressionVisitor<in TParameter, in TVariable, out TResult> :
+        IVisitor<Expression, TParameter, TVariable, TResult>
     {
         /// <summary>
         /// Visits a <see cref="MultiplicationExpression"/>.
         /// </summary>
         /// <param name="multiplicationExpression">The multiplication expression.</param>
-        /// <param name="parameter1">The parameter1.</param>
-        /// <param name="parameter2">The parameter2.</param>
-        void Multiply(MultiplicationExpression multiplicationExpression, TImmutableParameter parameter1, TMutableParameter parameter2);
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="variable">The variable.</param>
+        void Multiply(MultiplicationExpression multiplicationExpression, TParameter parameter, TVariable variable);
 
         /// <summary>
         /// Visits a <see cref="DivisionExpression"/>.
         /// </summary>
         /// <param name="divisionExpression">The division expression.</param>
-        /// <param name="parameter1">The parameter1.</param>
-        /// <param name="parameter2">The parameter2.</param>
-        void Divide(DivisionExpression divisionExpression, TImmutableParameter parameter1, TMutableParameter parameter2);
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="variable">The variable.</param>
+        void Divide(DivisionExpression divisionExpression, TParameter parameter, TVariable variable);
 
         /// <summary>
         /// Visits a <see cref="MagnitudeExpression"/>.
         /// </summary>
         /// <param name="magnitudeExpression">The magnitude expression.</param>
-        /// <param name="parameter1">The parameter1.</param>
-        /// <param name="parameter2">The parameter2.</param>
-        void Magnitude(MagnitudeExpression magnitudeExpression, TImmutableParameter parameter1, TMutableParameter parameter2);
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="variable">The variable.</param>
+        void Magnitude(MagnitudeExpression magnitudeExpression, TParameter parameter, TVariable variable);
 
         /// <summary>
         /// Visits a <see cref="ParenthesisExpression"/>.
         /// </summary>
         /// <param name="parenthesisExpression">The parentheses expression.</param>
-        /// <param name="parameter1">The parameter1.</param>
-        /// <param name="parameter2">The parameter2.</param>
-        void Parenthesis(ParenthesisExpression parenthesisExpression, TImmutableParameter parameter1, TMutableParameter parameter2);
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="variable">The variable.</param>
+        void Parenthesis(ParenthesisExpression parenthesisExpression, TParameter parameter, TVariable variable);
 
         /// <summary>
         /// Visits a <see cref="UnitExpression"/>.
         /// </summary>
         /// <param name="unitExpression">The unit expression.</param>
-        /// <param name="parameter1">The parameter1.</param>
-        /// <param name="parameter2">The parameter2.</param>
-        void Unit(UnitExpression unitExpression, TImmutableParameter parameter1, TMutableParameter parameter2);
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="variable">The variable.</param>
+        void Unit(UnitExpression unitExpression, TParameter parameter, TVariable variable);
 
         /// <summary>
         /// Visits a <see cref="VariableExpression"/>.
         /// </summary>
         /// <param name="variableExpression">The variable expression.</param>
-        /// <param name="parameter1">The parameter1.</param>
-        /// <param name="parameter2">The parameter2.</param>
-        void Variable(VariableExpression variableExpression, TImmutableParameter parameter1, TMutableParameter parameter2);
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="variable">The variable.</param>
+        void Variable(VariableExpression variableExpression, TParameter parameter, TVariable variable);
 
         /// <summary>
         /// Visits a <see cref="ConstantExpression"/>.
         /// </summary>
         /// <param name="constantExpression">The constant expression.</param>
-        /// <param name="parameter1">The parameter1.</param>
-        /// <param name="parameter2">The parameter2.</param>
-        void Constant(ConstantExpression constantExpression, TImmutableParameter parameter1, TMutableParameter parameter2);
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="variable">The variable.</param>
+        void Constant(ConstantExpression constantExpression, TParameter parameter, TVariable variable);
     }
 }
