288f944456d8e5dd4059cc8fae16b38037f3f563
Kim Hugener-Ohlsen <kho@hukano.com>
2021-06-19 01:01:45 +0200

    Updated dependencies, updated GHA

--- Source/Sundew.Quantities/Properties/AssemblyInfo.cs
+++ Source/Sundew.Quantities/Properties/AssemblyInfo.cs
@@ -4,18 +4,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
-using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyCopyright("Copyright © Hukano 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
 [assembly: InternalsVisibleTo("Sundew.Quantities.AcceptanceTests")]
 [assembly: InternalsVisibleTo("Sundew.Quantities.UnitTests")]
-[assembly: ComVisible(false)]
